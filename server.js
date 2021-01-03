1cdf6479ba3f5814724c70d94cdbcde88b34458a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-02 22:30:28 -0600

    Clase 11

--- server.js
+++ server.js
@@ -18,23 +18,33 @@ router.get('/', (req, res) => {
 	console.log(req.headers)
 	console.log(req.body)
 	console.log(req.query)
-	res.send('Hla desde get')
+	res.header({
+		Valor: 'Personalizado',
+	})
+	res.status(201).send('Hla desde get')
 })
 
 router.post('/', (req, res) => {
 	console.log(req.headers)
 	console.log(req.body)
 	console.log(req.query)
+	res.header({
+		Valor: 'Personalizado',
+	})
 	res.send('Hola desde post')
+	res.status(200).send('HOla desde post')
 })
 
 router.delete('/', (req, res) => {
 	console.log(req.headers)
 	console.log(req.body)
 	console.log(req.query)
-	res.send('HOla desde delete')
+	res.header({
+		Valor: 'Personalizado',
+	})
+	res.status(403).send({ error: 404, access: 'forbidden' })
 })
 
-//Empezando el servidor en el puerto 3000
+//Empezando el servidor en el puerto $port
 app.listen(app.get('port'))
 console.log(`La aplicacion está corriendo en el puerto ${app.get('port')}`)
